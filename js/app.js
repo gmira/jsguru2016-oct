774074340eb061cc12357449297458431dd4a9c4
Alumno11K3 <Alumno11K3@AulasKnowtech.es>
2016-10-25 21:05:14 +0200

    hoy

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-debugger;
+import "./oop.js";
+
+function A(){
+  this.x = 11;
+};
+
+A.prototype = {
+
+  doStuff: function(){
+    //this... esto apunta a la instancia que se cree mas adelante
+    console.log("hey");
+  }
+};
+
+
+var instanceOfA = new A();
+
+if(instanceOfA.hasOwnProperty("doStuff")){//mira si esta en su propio prototype
+
+}
+
+if("doStuff" in instanceOfA){//mira en toda la cadena de prototype 
+
+}
+//PROTOTYPE CHAIN
+instanceOfA.doStuff();
+
+console.log(instanceOfA.x);
+
+
+function B(){}
+//B.prototype = new A();// se añade todo lo que haya en el constructor (no es correcto)
+B.prototype = Object.create(A.prototype);//solo se añade el prototype (es correcto)
+
+var instanceOfB = new B();
+instanceOfB.doStuff();
---
+++ js/app.js.1
@@ -0,0 +1,34 @@
+"use strict";
+
+
+for (let i = 0; i < 10; i++) {
+
+}
+
+var func1 = (text, text2) => console.log(`${text},
+  ${text2}`);
+func1("hola mundo", "jeje");
+
+console.log(i);
+
+
+function A(){
+  this.x = "hola";
+  this.version = "1.0";
+}
+//Factory
+A.create = () => {
+  var instance = new A();
+
+if("init" in instance){
+  instance.init();
+}
+  instance.version = "1.0";
+};
+var a = A.create();
+
+
+//equals
+A.equals = (a, b) => {
+  return a.x == b.x;
+}
